a01c15d896f3beca5c2c912d3ad49482f7c95f04
noor <noormahdi205@gmail.com>
2024-01-03 11:22:42 +0300

    bootstrap, exhibition, cars of each exhibition, add car "

--- src/App.js
+++ src/App.js
@@ -1,32 +1,30 @@
-import React, { useEffect, useState } from 'react'
-import './App.css';
-import SignInForm from './components/user/SignInForm';
-import SignUpForm from './components/user/SignUpForm';
-import {Routes, Route, Link} from 'react-router-dom'
-import Axios from 'axios'
-import {jwtDecode} from 'jwt-decode'
-import AddCarForm from './components/forms/AddCarForm';
-import AddExhibitionForm from './components/forms/AddExhibitionForm';
-import BookingForm from './components/forms/BookingForm';
-import RequestForm from './components/forms/RequestForm';
-import ExhibitionPage from './components/exhibitionView/exhibitions';
-import ExhibitionCarsPage from './components/exhibitionView/carsOfEachExhibitions'
-import RequestList from './components/adminView/RequestList';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import SignInForm from "./components/user/SignInForm";
+import SignUpForm from "./components/user/SignUpForm";
+import { Routes, Route, Link } from "react-router-dom";
+import Axios from "axios";
+import { jwtDecode } from "jwt-decode";
+import AddCarForm from "./components/forms/AddCarForm";
+import AddExhibitionForm from "./components/forms/AddExhibitionForm";
+import BookingForm from "./components/forms/BookingForm";
+import RequestForm from "./components/forms/RequestForm";
+import ExhibitionPage from "./components/exhibitionView/exhibitions";
+import ExhibitionCarsPage from "./components/exhibitionView/carsOfEachExhibitions";
+import RequestList from "./components/adminView/RequestList";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 // import { Exhibition } from '../../Voiture/models/Exhibition';
 
 function App() {
-  
   const [isAuth, setIsAuth] = useState(false);
-  const [user, setUser]= useState({});
-
+  const [user, setUser] = useState({});
 
   useEffect(() => {
     const user = setUser();
     console.log(user);
 
-    if(user){
+    if (user) {
       setIsAuth(true);
       setUser(user);
     } else {
@@ -34,104 +32,169 @@ function App() {
       setIsAuth(false);
       setUser(null);
     }
-  }, [])
-  
+  }, []);
+
   const registerHandler = (user) => {
     Axios.post("auth/signup", user)
-    .then(res => {
-      console.log(res)
-    })
+      .then((res) => {
+        console.log(res);
+      })
 
-    .catch(err => {
-      console.log(err)
-    })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
   const loginHandler = (cred) => {
     Axios.post("auth/signin", cred)
-    .then(res => {
-      console.log(res.data.token);
+      .then((res) => {
+        console.log(res.data.token);
 
-      //Makes sure the token is Valid
-      let token = res.data.token;
+        //Makes sure the token is Valid
+        let token = res.data.token;
 
-      if(token != null){
-        localStorage.setItem("token", token);
-        const user = getUser();
-        console.log(user);
-        user ? setIsAuth(true): setIsAuth(false)
-        user ? setUser(user): setUser(null)
-      }
-    })
+        if (token != null) {
+          localStorage.setItem("token", token);
+          const user = getUser();
+          console.log(user);
+          user ? setIsAuth(true) : setIsAuth(false);
+          user ? setUser(user) : setUser(null);
+        }
+      })
 
-    .catch(err => {
-      console.log(err)
-    })
-  }
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  const getUser = ()=> {
+  const getUser = () => {
     const token = getToken();
-    return token ? jwtDecode(token).user : null
-  }
+    return token ? jwtDecode(token).user : null;
+  };
 
-  const getToken =()=>{
+  const getToken = () => {
     const token = localStorage.getItem("token");
     return token;
-  }
+  };
 
-  const onLogoutHandler =(e) => {
+  const onLogoutHandler = (e) => {
     e.preventDefault();
     localStorage.removeItem("token");
     setIsAuth(false);
     setUser(null);
-  }
+  };
 
   return (
     <div className="App">
-       <div className="px-3 py-2 text-bg-dark border-bottom text-right header">
-      <nav>
-      <div className="container d-flex justify-content-end">
-        {isAuth ? (
-          <div>
-            <Link className='nav-link text-white d-inline' to="/">Home</Link> &nbsp;
-            <Link className='nav-link text-white d-inline' to="/exhibition/index">Exhibition</Link>&nbsp;
-            <Link className='nav-link text-white d-inline' to="/request/add">Submit Request</Link>&nbsp;
-            <Link className='nav-link text-white d-inline' to="/request/index"> Request List</Link>&nbsp;
-            <Link className='nav-link text-white d-inline' to="/logout" onClick={onLogoutHandler}>Logout</Link> &nbsp;
+      <div className="px-3 py-2 text-bg-dark border-bottom text-right header">
+        <nav>
+          <div className="container d-flex justify-content-end">
+            {isAuth ? (
+              <div>
+                <Link className="nav-link text-white d-inline" to="/">
+                  Home
+                </Link>{" "}
+                &nbsp;
+                <Link
+                  className="nav-link text-white d-inline"
+                  to="/exhibition/index"
+                >
+                  Exhibition
+                </Link>
+                &nbsp;
+                <Link
+                  className="nav-link text-white d-inline"
+                  to="/request/add"
+                >
+                  Submit Request
+                </Link>
+                &nbsp;
+                <Link
+                  className="nav-link text-white d-inline"
+                  to="/request/index"
+                >
+                  {" "}
+                  Request List
+                </Link>
+                &nbsp;
+                <Link
+                  className="nav-link text-white d-inline"
+                  to="/logout"
+                  onClick={onLogoutHandler}
+                >
+                  Logout
+                </Link>{" "}
+                &nbsp;
+              </div>
+            ) : (
+              <div>
+                <Link className="nav-link text-white d-inline" to="/">
+                  Home
+                </Link>{" "}
+                &nbsp;
+                <Link
+                  className="nav-link text-white d-inline"
+                  to="/exhibition/index"
+                >
+                  Exhibition
+                </Link>
+                &nbsp;
+                <Link className="nav-link text.-white d-inline" to="/signup">
+                  Sign Up
+                </Link>{" "}
+                &nbsp;
+                <Link className="nav-link text-white d-inline" to="/signin">
+                  Sign In
+                </Link>{" "}
+                &nbsp;
+              </div>
+            )}
           </div>
-        ) : (
-          <div>
-            <Link className='nav-link text-white d-inline' to="/">Home</Link> &nbsp;
-            <Link className='nav-link text-white d-inline' to="/exhibition/index">Exhibition</Link>&nbsp;
-            <Link className='nav-link text-white d-inline' to="/signup">Sign Up</Link> &nbsp;
-            <Link className='nav-link text-white d-inline' to="/signin">Sign In</Link> &nbsp;
-          </div>
-        )}
-        </div>
-      </nav>
-  </div>
+        </nav>
+      </div>
 
       <div>
         <Routes>
           <Route path="/" />
-          <Route path='/exhibition/index' element={<ExhibitionPage></ExhibitionPage>} />
-          <Route path="/signup" element={<SignUpForm register={registerHandler} />} />
-          <Route path="/signin" element={isAuth ? ( <AddCarForm />) : (<SignInForm login={loginHandler} /> )}/>
-        {/* Add car acting as homepage in this scenario */}
-        <Route path="/request/add" element={<RequestForm user={user}></RequestForm>} />
-        <Route path='/request/index' element={<RequestList user={user}></RequestList>} />
-        {/* <Route path='' element={} /> */}
-         {/* <Route path='' element={} /> */}
+          <Route
+            path="/exhibition/index"
+            element={<ExhibitionPage></ExhibitionPage>}
+          />
+          <Route
+            path="/exhibition/cars/:id"
+            element={<ExhibitionCarsPage></ExhibitionCarsPage>}
+          />
+          <Route path="car/add" element={<AddCarForm />} />
+          <Route
+            path="/signup"
+            element={<SignUpForm register={registerHandler} />}
+          />
+          <Route
+            path="/signin"
+            element={
+              isAuth ? <AddCarForm /> : <SignInForm login={loginHandler} />
+            }
+          />
+          {/* Add car acting as homepage in this scenario */}
+          <Route
+            path="/request/add"
+            element={<RequestForm user={user}></RequestForm>}
+          />
+          <Route
+            path="/request/index"
+            element={<RequestList user={user}></RequestList>}
+          />
+          {/* <Route path='' element={} /> */}
+          {/* <Route path='' element={} /> */}
           {/* <Route path='' element={} /> */}
         </Routes>
       </div>
-     
+
       <footer className="px-3 py-2 text-bg-dark mt-5 stickToBottom">
-      <div className="container">
-    <p className="mb-1 text-white">&copy; 2024 | Voiture App </p>
-</div>
+        <div className="container">
+          <p className="mb-1 text-white">&copy; 2024 | Voiture App </p>
+        </div>
       </footer>
-  
     </div>
   );
 }
--- src/components/exhibitionView/carsOfEachExhibitions.jsx
+++ src/components/exhibitionView/carsOfEachExhibitions.jsx
@@ -1,16 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
+import { Link , useParams} from 'react-router-dom';
 
-function ExhibitionCarsPage({ exhibitionId }) {
+function ExhibitionCarsPage( ) {
   const [cars, setCars] = useState([]);
-exhibitionId="65915f0f35e4ce658ac13cff"
+  const [exhibition, setExhibition] = useState([]);
+  const { id: exhibitionId } = useParams(); // Get the exhibitionId from the URL parameter
+
   useEffect(() => {
     // Fetch cars for the specific exhibition
     axios
       .get(`/exhibition/detail?id=${exhibitionId}`)
       .then(response => {
         console.log(response);
-        setCars(response.data.exhibition);
+        setCars(response.data.exhibition?response.data.exhibition.Car:[]);
+        setExhibition(response.data.exhibition?response.data.exhibition:[]);
       })
       .catch(error => {
         console.error('Error fetching cars:', error);
@@ -18,21 +22,26 @@ exhibitionId="65915f0f35e4ce658ac13cff"
   }, [exhibitionId]);
 
   return (
-    <div>
-      <h1>Cars for Exhibition</h1>
-      {exhibitionId}
-      {/* {cars.map(car => ( */}
-        {/* <div key={cars._id}> */}
-          {/* <h2>{cars.car_name}</h2>
-          <p>{cars.car_description}</p>
-          <p>Company: {cars.car_company}</p>
-          <p>Model: {cars.car_model}</p>
-          <p>Price: {cars.car_price}</p>
-          <img src={cars.car_avatar} alt={cars.car_name} /> */}
-          {/* Display other car information as needed */}
-        {/* </div> */}
-      {/* ))} */}
+
+
+<div>
+  <div className="d-flex justify-content-end mb-3">
+    <Link to="/AddCarForm" className="btn btn-secondary">Add Car</Link>
+  </div>
+  <h1>Cars for Exhibition</h1>
+  {exhibitionId}
+  {cars.map(car => (
+    <div key={car._id}> {/* Use "car" instead of "cars" */}
+      <h2>{car.car_name}</h2> {/* Use "car" instead of "cars" */}
+      <p>{car.car_description}</p> {/* Use "car" instead of "cars" */}
+      <p>Company: {car.car_company}</p> {/* Use "car" instead of "cars" */}
+      <p>Model: {car.car_model}</p> {/* Use "car" instead of "cars" */}
+      <p>Price: {car.car_price}</p> {/* Use "car" instead of "cars" */}
+      <img src={car.car_avatar} alt={car.car_name} /> {/* Use "car" instead of "cars" */}
+      {/* Display other car information as needed */}
     </div>
+  ))}
+</div>
   );
 }
 
